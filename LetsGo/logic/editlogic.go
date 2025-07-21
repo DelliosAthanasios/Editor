4a99a24c82acb83cf3036dae584c867437362442
Dellios <nasosdellios@gmail.com>
2025-07-21 10:51:00 +0300

    new beginnings

---
+++ LetsGo/logic/editlogic.go
@@ -0,0 +1,32 @@
+/* Here add the main logic of the application
+let me list it for you:
+New File = Creates a new file
+Open file = Opens an existing file
+Save File = Saves the current file
+Duplicate File = Duplicates the current file
+Save all = Saves all open files
+Close File = Closes the current file
+Edit File = Edits the current file
+Delete File = Deletes the current file
+*/
+/*
+Then we have text manipulation functions:
+ undo = Undoes the last action
+ redo = Redoes the last undone action
+ Select All = Selects all text in the current file
+ Go to Start of a line = Moves the cursor to the start of the current line
+ Go to End of a line = Moves the cursor to the end of the current line
+ Go to Start of a file = Moves the cursor to the start of the file
+ Go to End of a file = Moves the cursor to the end of the file
+ go to Next word = Moves the cursor to the next word
+ go to Previous word = Moves the cursor to the previous word
+ delete word = Deletes the word at the cursor position
+ delete line = Deletes the current line
+ go forward one character = Moves the cursor one character forward
+ go backward one character = Moves the cursor one character backward
+ go to next line = Moves the cursor to the next line
+ go to previous line = Moves the cursor to the previous line
+*/
+/*
+ we will continue with more text manipulation functions in the future
+*/
---
+++ LetsGo/logic/searching.go
@@ -0,0 +1,14 @@
+/*
+Here will be the logic of the searching functionality in the application.
+We will implement a search function that allows users to fin text within the current file.
+The search function will take
+1) a search term as input
+2) search through the text of the current file ( Let the user deside which algorithm to use, like linear search or regexm or binary search by default use the faster one)
+3) Highlite the found text in the file
+4) if the user allows it add a replace function that will replace the found text with a new text
+5) The user can replace all or just one instance of the found text
+6) The user can navigate through the found instances of the text
+7) The user can cancel the search operation at any time
+8) The search function will be case sensitive or insensitive based on user preference
+9) The search function will support regular expressions for advanced searching
+*/
